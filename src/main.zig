4726bb016ea01b898808367b42f6223246bdcfda
D-Berg <danielfjberg@gmail.com>
2025-02-06 11:13:03 +0100

    open window

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,31 @@
 
 const std = @import("std");
 
+const zgl = @import("zgl");
+const glfw = zgl.glfw;
+const wgpu = zgl.wgpu;
+
+const WINDOW_WIDTH = 600;
+const WINDOW_HEIGHT = 600;
+
 pub fn main() !void {
 
-    std.debug.print("hi", .{});
+    try glfw.init();
+    defer glfw.terminate();
+
+    glfw.Window.hint(.{ .resizable = false, .client_api = .NO_API });
+    const window = try glfw.Window.Create(WINDOW_WIDTH, WINDOW_HEIGHT, "Raytracing!");
+    defer  window.destroy();
+
+
+    while (!window.ShouldClose()) {
+
+        glfw.pollEvents();
+
+    }
+
+
+
 
 }
 
